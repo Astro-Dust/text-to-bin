9c74611bae54c9c55f7a1c0dbc7f454cda35d9bc
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-05-28 16:21:07 -0300

    text converted to binary

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from tkinter import filedialog
 import tkinter as tk
+import os
+
+current_path = os.getcwd()
 
 def text_to_bin():
     filename = filedialog.askopenfile()
     if filename:
         filename = str(filename).split(' ')[1].replace('name=', '')
         final_filename = filename.replace("'", '')
-        print(final_filename)
+        return final_filename
 
     else:
         print('No file selected.')
@@ -16,6 +19,14 @@ root.title('Text to binary translator')
 
 button = tk.Button(root, text='Search txt file', command=text_to_bin)
 button.pack(pady=20)
+
+txt_file = text_to_bin()
+
+with open(txt_file) as file:
+    for line in file:
+        line_bin = ' '.join(format(ord(char), '08b') for char in line.strip())
+        print(line_bin)
+file.close()
 
 root.mainloop()
 
